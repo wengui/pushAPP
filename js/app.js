9432e19e3324a79753dfa1a9ac2049cdd51a2be6
zmj <zmj@zmj-PC>
2016-03-14 11:34:50 +0800

    弹出等待窗修改 服务器ip地址统合

--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@
 // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)   
 // 例子：   
 // (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423   
-// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18   
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18  
+
+
+//API地址常量
+var API_ADDRESS = 'http://192.168.1.162:8080/pushApi/'; //定义了常量
+
+
 Date.prototype.Format = function(fmt) { //author: meizz   
 		var o = {
 			"M+": this.getMonth() + 1, //月份   
@@ -23,9 +29,10 @@ Date.prototype.Format = function(fmt) { //author: meizz
 	}
 	//根据病人名字和时间显示通知详情ajax
 function info(name, time) {
+	plus.nativeUI.showWaiting("正在加载");
 	console.log(name);
 	console.log(time);
-	mui.ajax('http://202.120.117.54:8080/pushApi/report/reportResult.do', {
+	mui.ajax(API_ADDRESS + 'report/reportResult.do', {
 		data: {
 			//						pname: "张三",
 			//						ptime: "2016-03-08 13:49:25"
@@ -34,9 +41,10 @@ function info(name, time) {
 		},
 		dataType: 'json', //服务器返回json格式数据
 		type: 'post', //HTTP请求类型
-		timeout: 100000, //超时时间设置为10秒；
+		timeout: 5000, //超时时间设置为10秒；
 		success: function(data) {
 			//服务器返回响应，根据响应结果，分析是否登录成功；
+			plus.nativeUI.closeWaiting();
 			console.log("success");
 			console.log(data.result);
 			mui.openWindow({
@@ -49,6 +57,7 @@ function info(name, time) {
 			});
 		},
 		error: function(xhr, type, errorThrown) { //异常处理；
+			plus.nativeUI.closeWaiting();
 			mui.alert('数据服务器链接失败', '错误', function() {
 
 			});
@@ -58,17 +67,17 @@ function info(name, time) {
 
 //ios用户启动将token上传服务器
 function tokenUpload(token) {
-	mui.ajax('http://202.120.117.54:8080/pushApi/deviceTokenAction/updateDeviceToken.do', {
+	mui.ajax(API_ADDRESS + 'deviceTokenAction/updateDeviceToken.do', {
 		data: {
 			deviceToken: token
 		},
 		dataType: 'json', //服务器返回json格式数据
 		type: 'post', //HTTP请求类型
-		timeout: 100000, //超时时间设置为10秒；
+		timeout: 5000, //超时时间设置为10秒；
 		success: function(data) {
 			localStorage.setItem("runFirst", "Yes");
 			console.log("设置后" + localStorage.getItem("runFirst"));
-			
+
 		},
 		error: function(xhr, type, errorThrown) { //异常处理；
 			mui.alert('数据服务器链接失败', '错误', function() {
@@ -78,15 +87,13 @@ function tokenUpload(token) {
 	});
 }
 
-
 //ios用户启动将token上传服务器
 function pageJump(page) {
 	mui.openWindow({
-				url: page,
-				id: page,
-				extras: {
-
-				}
-			});
+		url: page,
+		id: page,
+		waiting: {
+			autoShow: false //自动显示等待框，默认为true
+		}
+	});
 }
-
